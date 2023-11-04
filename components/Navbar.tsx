727244e7c1623216e1903fa736d42d8592ac1818
Rohan Gope <rg2512@it.jgec.ac.in>
2023-11-04 21:43:23 +0530

    Add landing page and update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import MenuIcon from "./MenuIcon";
 
 const Navbar = () => {
+  const [loggedin,setLoggedin] = useState(false)
   return (
     <div className="w-full px-5 py-2 flex justify-between items-center bg-bg_dark_secondary text-white shadow-md">
       <Link href="/">
@@ -15,10 +17,18 @@ const Navbar = () => {
           className="object-cover"
         />
       </Link>
-      <Link href="/login">
-        Login
-      </Link>
-      <MenuIcon theme="dark" />
+      <div className="flex flex-row gap-4">
+        <Link href="/pitches" className="hover:underline">
+          Pitches  
+        </Link>
+        {
+          loggedin ? <h1>User</h1> : <Link href="/login" className="hover:underline ">
+              Login
+            </Link>
+        }
+        
+        <MenuIcon theme="dark" />
+      </div>
     </div>
   );
 };
