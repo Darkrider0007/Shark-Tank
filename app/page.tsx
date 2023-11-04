defb8e2114d82b057be2f96ff7f91315790d7dd8
Rohan Gope <rg2512@it.jgec.ac.in>
2023-11-04 20:11:21 +0530

    Add Navbar component to layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
-      <Navbar/>
+      
       <h1>Next.js + TypeScript</h1>
       <Link href="/login">Login</Link>
     </>
