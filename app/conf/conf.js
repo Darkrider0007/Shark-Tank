8a626be506c9e22be10c0d650d34f15ffbc7ea16
Rohan Gope <rg2512@it.jgec.ac.in>
2023-11-15 02:01:48 +0530

    Adding Mail.js

--- app/conf/conf.js
+++ app/conf/conf.js
@@ -6,6 +6,9 @@ const conf = {
     appwriteBucketId: String(process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID),
     appwriteUserDatabaseID: String(process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID_USERS),
     appwriteUserCollectionID: String(process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID_USERS),
+    emailjsServiceID: String(process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID),
+    emailjsPublicKey : String(process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY),
+    emailjsTemplateId : String(process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID)
 }
 
 export default conf
--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,10 +1,32 @@
 "use client";
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
 import { SubmitButton } from "@/components";
-
+import { useForm } from "react-hook-form";
+import emailjs from '@emailjs/browser';
+import conf from "../conf/conf";
 
 const Page = () => {
+  const { register, handleSubmit } = useForm();
+  const form = useRef<HTMLFormElement | null>(null);
+  const submitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  
+    try {
+      // Pass the form element directly to sendForm
+      await emailjs.sendForm(
+        conf.emailjsServiceID,
+        conf.emailjsTemplateId,
+        event.currentTarget, // Access the form element from the event
+        conf.emailjsPublicKey
+      );
+  
+      console.log('Email sent successfully');
+    } catch (error) {
+      console.error('Error sending email:', error);
+    }
+  };
+  
   return (
     <div className="min-h-screen w-full bg-bg_dark_primary flex justify-center items-center text-[#fefefe]">
       <div className="max-w-[600px] w-full h-screen bg-bg_dark_secondary shadow-lg p-4">
@@ -18,18 +40,18 @@ const Page = () => {
           />
         </div>
 
-        <form className="flex flex-col justify-start items-start w-full mt-2">
+        <form className="flex flex-col justify-start items-start w-full mt-2"
+         ref={form}
+          onSubmit={submitHandler}
+        >
           <div className="flex flex-col justify-between items-start mb-4 w-full">
             <label htmlFor="name">Name</label>
             <div className="flex justify-between items-center w-full p-2 bg-transparent border border-[#fefefe] mt-1">
               <input
                 type="text"
                 id="name"
-                name="name"
                 placeholder="Enter Your name"
                 className="bg-transparent focus:outline-none w-full"
-                // value={}
-                // onChange={}
                 required
               />
             </div>
@@ -41,11 +63,8 @@ const Page = () => {
               <input
                 type="email"
                 id="email"
-                name="email"
                 placeholder="Enter Your email"
                 className="bg-transparent focus:outline-none w-full"
-                // value={}
-                // onChange={}
                 required
               />
             </div>
@@ -63,8 +82,8 @@ const Page = () => {
               />
             </div>
           </div>
-
-          <SubmitButton props="Send"/>
+          <input type="submit" value="Send" className="px-3 py-1.5 border border-[#fefefe] mt-4 rounded-lg hover:bg-white hover:text-black hover:px-3.2 hover:py-1.7" />
+          {/* <SubmitButton props="Send"/> */}
         </form>
       </div>
     </div>
